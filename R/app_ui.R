8cb95172c6ae137bd82171c508d942c40dbc845a
i2z1 <i2z1@yandex.ru>
2021-08-22 18:44:23 +0300

    add 1st tab input layout

--- R/app_ui.R
+++ R/app_ui.R
@@ -13,10 +13,37 @@ app_ui <- function(request) {
                tabPanel("Monitoring",
                         sidebarLayout(
                           sidebarPanel(
-                            dateInput()
+                            # dateInput("date_input", "Date"),
+                            sliderInput("temp_input",
+                                        label = "Body Temperature",
+                                        min = 35,
+                                        max = 41,
+                                        value = 36.6,
+                                        step = 0.1),
+                            sliderInput("bpres_input",
+                                        label = "Blood Pressure",
+                                        min = 40,
+                                        max = 200,
+                                        value = c(80,120),
+                                        step = 1),
+                            sliderInput("satur_input",
+                                        label = "Saturation",
+                                        min = 90,
+                                        max = 100,
+                                        value = 97,
+                                        step = 1),
+                            numericInput("breath_input",
+                                         label = "Breaths per minute",
+                                         value = 14),
+                            numericInput("pulse_input",
+                                         label = "Pulse",
+                                         value = 70),
+                            actionButton("submit1_input",
+                                         label = "Submit")
                           ),
                           mainPanel()
                         )),
+               tabPanel("Blood"),
                tabPanel("About")
 
     )
