f5b8e8bca66bdccf01b28ca72deb105a8fcf9b9a
i2z1 <i2z1@yandex.ru>
2021-08-22 18:17:42 +0300

    init layout

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,6 +1,6 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
@@ -8,27 +8,36 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # Your application UI logic 
-    fluidPage(
-      h1("MediWatch")
+    # Your application UI logic
+    navbarPage("MediWatch",
+               tabPanel("Monitoring",
+                        sidebarLayout(
+                          sidebarPanel(
+                            dateInput()
+                          ),
+                          mainPanel()
+                        )),
+               tabPanel("About")
+
     )
+
   )
 }
 
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function(){
-  
+
   add_resource_path(
     'www', app_sys('app/www')
   )
- 
+
   tags$head(
     favicon(),
     bundle_resources(
@@ -36,7 +45,7 @@ golem_add_external_resources <- function(){
       app_title = 'MediWatch'
     )
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
   )
 }
 
