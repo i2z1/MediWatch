726c94070a53afe92dc9d4659452a4102141260c
i2z1 <i2z1@yandex.ru>
2021-08-25 16:59:13 +0300

    adjustments for blood presure ggplot

--- R/mod_graph_pressure.R
+++ R/mod_graph_pressure.R
@@ -40,13 +40,16 @@ mod_graph_pressure_server <- function(id,healthdata){
           x = "Systolic Pressure (mmHg)",
           y = "Diastolic Pressure (mmHg)"
         ) +
-        expand_limits(x = 0, y = 0) +
+        expand_limits(x = 60, y = 40) +
         expand_limits(x = 250, y = 150) +
-        geom_rect(xmin = 0, xmax = 250, ymin = 0, ymax = 150, fill = "#E40B06", color=NA, alpha = 1) +#Grade 3
-        geom_rect(xmin = 0, xmax = 179, ymin = 0, ymax = 109, fill = "#ED7E08", color=NA, alpha = 1) +#Grade 2
-        geom_rect(xmin = 0, xmax = 159, ymin = 0, ymax = 99, fill = "#F8BF78", color=NA, alpha = 1) +#Grade 1
-        geom_rect(xmin = 0, xmax = 139, ymin = 0, ymax = 89, fill = "#BAFDB6", color=NA, alpha = 1) +#Normal
-        geom_rect(xmin = 0, xmax = 129, ymin = 0, ymax = 84, fill = "#BAFDB6", color=NA, alpha = 1) +#Optimal
+        scale_x_continuous(breaks = seq(60,250,10)) +
+        scale_y_continuous(breaks = seq(40,150,10)) +
+        geom_rect(xmin = 0, xmax = 250, ymin = 0, ymax = 150, fill = "#E40B06", color=NA, alpha = 0.2) + # Ishemia
+        geom_rect(xmin = 70, xmax = 230, ymin = 50, ymax = 130, fill = "#ed6208", color=NA, alpha = 0.2) +#Grade 3
+        geom_rect(xmin = 70, xmax = 179, ymin = 50, ymax = 109, fill = "#ED7E08", color=NA, alpha = 1) +#Grade 2
+        geom_rect(xmin = 70, xmax = 159, ymin = 50, ymax = 99, fill = "#F8BF78", color=NA, alpha = 1) +#Grade 1
+        geom_rect(xmin = 70, xmax = 129, ymin = 50, ymax = 84, fill = "#f6b4fd", color=NA, alpha = 1) +#Hypotension
+        geom_rect(xmin = 90, xmax = 129, ymin = 60, ymax = 84, fill = "#BAFDB6", color=NA, alpha = 1) +#Optimal
         #p <- p + scale_color_manual(values=c("darkblue", "lightblue"))
         geom_point()
     })
