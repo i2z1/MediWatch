62e8422d86de3e41775f43dab39a37005d075772
Chiefff <i2z1@yandex.ru>
2021-08-26 13:30:29 +0300

    add func for data update from common health params

--- R/app_server.R
+++ R/app_server.R
@@ -11,6 +11,16 @@ app_server <- function( input, output, session ) {
   mod_graph_pressure_server("graph_pressure_ui_1",tdf)
   mod_graph_linear_lim_server("graph_linear_lim_ui_1",tdf, tdf$Temp, c(35,41), "Time", "Temperature")
 
+
+  observeEvent( input$submit1_input, {
+    data_list <- list(temp = input$temp_input,
+                      bpres = input$bpres_input,
+                      satur = input$satur_input,
+                      bpm = input$breath_input,
+                      pulse = input$pulse_input,
+                      senses = input$senses_input)
+    update_data(data_list)
+  })
   #  output$test <- renderText({print(input$bpres_input[2])})
 
 }
---
+++ R/fct_update_data.R
@@ -0,0 +1,10 @@
+#' update_data
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+update_data <- function(ndf){
+  print(ndf)
+}
